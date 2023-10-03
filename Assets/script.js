b02b02a50de1c7a4da4bffcdc5e8ef7fb8b00bce
Mike <mreickcastillero@gmail.com>
2023-10-02 22:52:46 -0700

    added color changing aspect with the current time, as well as added date and time to top of page

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,152 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var saveBtn = $(".saveBtn");
+
+console.log(txtarea);
+
+$("#currentDay").text(dayjs().toDate())
+
+function nineAM (){
+var timeOf9 = 9;
+if (timeOf9 < dayjs().hour()){
+  $("#hour-9").addClass("past");
+}
+else if (timeOf9 === dayjs().hour()) {
+  $("#hour-9").addClass("present");
+}
+else if (timeOf9 > dayjs().hour()) {
+  $("#hour-9").addClass("future");
+}
+}
+
+nineAM()
+
+function tenAM (){
+var timeOf10 = 10;
+if (timeOf10 < dayjs().hour()){
+  $("#hour-10").addClass("past");
+}
+else if (timeOf10 === dayjs().hour()) {
+  $("#hour-10").addClass("present");
+}
+else if (timeOf10 > dayjs().hour()) {
+  $("#hour-10").addClass("future");
+}
+}
+
+tenAM()
+
+function elevenAM (){
+var timeOf11 = 11;
+if (timeOf11 < dayjs().hour()){
+  $("#hour-11").addClass("past");
+}
+else if (timeOf11 === dayjs().hour()) {
+  $("#hour-11").addClass("present");
+}
+else if (timeOf11 > dayjs().hour()) {
+  $("#hour-11").addClass("future");
+}
+}
+  
+elevenAM()
+
+function twelvePM (){
+var timeOf12 = 12;
+if (timeOf12 < dayjs().hour()){
+  $("#hour-12").addClass("past");
+}
+else if (timeOf12 === dayjs().hour()) {
+  $("#hour-12").addClass("present");
+}
+else if (timeOf12 > dayjs().hour()) {
+  $("#hour-12").addClass("future");
+}
+}
+    
+twelvePM()
+
+function onePM (){
+var timeOf1 = 1;
+if (timeOf1 < dayjs().hour()){
+  $("#hour-1").addClass("past");
+}
+else if (timeOf1 === dayjs().hour()) {
+  $("#hour-1").addClass("present");
+}
+else if (timeOf1 > dayjs().hour()) {
+  $("#hour-1").addClass("future");
+}
+}
+      
+onePM()
+
+function twoPM (){
+var timeOf2 = 2;
+if (timeOf2 < dayjs().hour()){
+  $("#hour-2").addClass("past");
+}
+else if (timeOf2 === dayjs().hour()) {
+  $("#hour-2").addClass("present");
+}
+else if (timeOf2 > dayjs().hour()) {
+  $("#hour-2").addClass("future");
+}
+}
+        
+twoPM()
+
+function threePM (){
+var timeOf3 = 3;
+if (timeOf3 < dayjs().hour()){
+  $("#hour-3").addClass("past");
+}
+else if (timeOf3 === dayjs().hour()) {
+  $("#hour-3").addClass("present");
+}
+else if (timeOf3 > dayjs().hour()) {
+  $("#hour-3").addClass("future");
+}
+}
+          
+threePM()
+
+function fourPM (){
+var timeOf4 = 4;
+if (timeOf4 < dayjs().hour()){
+  $("#hour-4").addClass("past");
+}
+else if (timeOf4 === dayjs().hour()) {
+  $("#hour-4").addClass("present");
+}
+else if (timeOf4 > dayjs().hour()) {
+  $("#hour-4").addClass("future");
+}
+}
+
+fourPM()
+
+function fivePM (){
+var timeOf5 = 5;
+if (timeOf5 < dayjs().hour()){
+  $("#hour-5").addClass("past");
+}
+else if (timeOf5 === dayjs().hour()) {
+  $("#hour-5").addClass("present");
+}
+else if (timeOf5 > dayjs().hour()) {
+  $("#hour-5").addClass("future");
+}
+}
+              
+fivePM()
+//----------------------------------------------------------------------------------------------
+saveBtn.on("click", function(){
+
+})
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,15 +155,14 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
 });
